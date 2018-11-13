3f8535e8f058aa3bd665faeefd77e9bf3cd8f015
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-13 15:54:17 +0000

    components(Footer): add MetaFooter component

---
+++ src/components/Footer/MetaFooter/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Copyright from "./Copyright";
+import PoweredBy from "./PoweredBy";
+
+export default (props) => (
+  <section css={{
+    display: "block",
+    margin: "4vw auto 0 auto",
+    padding: "0 4rem",
+    maxWidth: "128rem",
+  }}>
+    <div css={{
+      display: "flex",
+      paddingTop: "1.6rem",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderTop: "1px dashed #e5eff5",
+    }}>
+      <Copyright />
+      <PoweredBy />
+    </div>
+  </section>
+);
