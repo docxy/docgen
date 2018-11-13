1f8a241376b2a3bce4bcb7ddb5cb511b3a68a7b2
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-13 15:55:45 +0000

    components: add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import MetaFooter from "./MetaFooter";
+
+export default (props) => (
+  <footer css={{
+    display: "block",
+    padding: "4vw 0",
+    borderTop: "1px solid #e5eff5",
+    backgroundColor: "#ffffff",
+  }}>
+    <MetaFooter />
+  </footer>
+);
