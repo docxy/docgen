4a626020f1401a39ded980ed0ae622d94981715b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-13 17:02:06 +0000

    components(Header): add Logo component

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+import Link from "../Link";
+
+export default (props) => (
+  <StaticQuery
+    query={graphql`
+      query LogoQuery {
+        config: dataYaml {
+          title
+          favicon
+        }
+      }
+    `}
+    render={data => (
+      <div css={{
+        display: "flex",
+        position: "relative",
+        width: "22rem",
+        padding: "1.2rem 0",
+        paddingRight: "3.2rem",
+        alignItems: "center",
+      }}>
+        <Link to="/" css={{
+          position: "relative",
+        }}>
+          {
+            data.config.favicon
+            ? <img
+                src={ data.config.favicon }
+                height="25"
+                width="auto"
+                alt="Logo"
+                css={{
+                  display: "flex",
+                }}
+              />
+            : data.config.title
+          }
+        </Link>
+
+        <Link to="/" css={{
+          position: "relative",
+          marginLeft: "1.6rem",
+          paddingLeft: "1.6rem",
+          ":before": {
+            content: " ",
+            position: "absolute",
+            top: -3.5,
+            left: 1,
+            display: "block",
+            width: 1,
+            height: 23,
+            background: "#d3e4ee",
+            transform: "rotate(25deg)",
+          },
+        }}>
+          Docs
+        </Link>
+      </div>
+    )}
+  />
+);
