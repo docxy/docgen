79917a3c504456e60e53514290ea53cb85ec7d3a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-14 02:17:34 +0000

    layouts: add default layout

---
+++ src/layouts/default/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Main from "../../components/Main";
+
+import "./normalize.css";
+import "./index.css";
+
+export default (props) => (
+  <StaticQuery
+    query={graphql`
+      query SiteConfigQuery {
+        config: dataYaml {
+          title
+          description
+          image
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={ data.config.title }
+          description= { data.config.description }
+        />
+        <Header />
+        <Main>
+          { props.children }
+        </Main>
+        <Footer />
+      </>
+    )}
+  />
+);
