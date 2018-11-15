3c24972981ef464eb870a25afec09f2b8c13dfa4
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-15 05:57:57 +0000

    components(Main): use AwesomeData instead of GraphQL

--- src/components/Main/Sidebar.jsx
+++ src/components/Main/Sidebar.jsx
@@ -1,77 +1,58 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
 
 import Link from "../Link";
+import * as AwesomeData from "../AwesomeData";
 
 export default (props) => (
-  <StaticQuery
-    query={graphql`
-      query SidebarLinksQuery {
-        config: dataYaml {
-          color
-        }
-        allNavigationYaml {
-          links: edges {
-            node {
-              title
-              link
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div css={{
+  <div css={{
+    display: "block",
+    position: "relative",
+    width: "22rem",
+    paddingRight: "4rem",
+    flexShrink: 0,
+  }}>
+    <div css={{
+      marginRight: "-1.2rem",
+      position: "sticky",
+      top: "10rem",
+    }}>
+      <nav css={{
         display: "block",
-        position: "relative",
-        width: "22rem",
-        paddingRight: "4rem",
-        flexShrink: 0,
       }}>
-        <div css={{
-          marginRight: "-1.2rem",
-          position: "sticky",
-          top: "10rem",
+        <ol css={{
+          margin: 0,
+          padding: 0,
+          listStyle: "none",
         }}>
-          <nav css={{
-            display: "block",
-          }}>
-            <ol css={{
-              margin: 0,
-              padding: 0,
-              listStyle: "none",
-            }}>
-              {
-                data.allNavigationYaml.links.map(({ node }, i) => (
-                  <li key={ i } css={{
-                    fontSize: "1.45rem",
-                    lineHeight: "1.65rem",
-                    "& > a.active": {
-                      color: data.config.color || "#3eb0ef",
-                      fontWeight: 500,
-                    }
-                  }}>
-                    <Link
-                      className={ document.location.pathname === node.link ? "active" : "" }
-                      to={ node.link }
-                      css={{
-                        display: "inline-block",
-                        width: "100%",
-                        height: "100%",
-                        padding: "6px 0",
-                        color: "#738a94",
-                        lineHeight: "1.5em",
-                      }}
-                    >
-                      { node.title }
-                    </Link>
-                  </li>
-                ))
-              }
-            </ol>
-          </nav>
-        </div>
-      </div>
-    )}
-  />
+          {
+            AwesomeData.navigation.map((link, i) => (
+              <li key={ i } css={{
+                fontSize: "1.45rem",
+                lineHeight: "1.65rem",
+                "& > a.active": {
+                  color: AwesomeData.config.color || "#3eb0ef",
+                  fontWeight: 500,
+                }
+              }}>
+                <Link
+                  className={ document.location.pathname === link.link ? "active" : "" }
+                  to={ link.link }
+                  css={{
+                    display: "inline-block",
+                    width: "100%",
+                    height: "100%",
+                    padding: "6px 0",
+                    color: "#738a94",
+                    lineHeight: "1.5em",
+                  }}
+                >
+                  { link.title }
+                </Link>
+              </li>
+            ))
+          }
+        </ol>
+      </nav>
+    </div>
+  </div>
 );
