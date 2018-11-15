b2d065307cc9c956e14b2b4cf3faecc308d92276
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-15 05:40:12 +0000

    components: add AwesomeData

---
+++ src/components/AwesomeData.jsx
@@ -0,0 +1,6 @@
+import config from "../../data/config.yaml";
+import deploy from "../../data/deploy.yaml";
+import links from "../../data/links.yaml";
+import navigation from "../../data/navigation.yaml";
+
+export { config, deploy, links, navigation };
