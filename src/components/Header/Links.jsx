00cbb9a3250b12f43cb38e754e6029c69ea20e8d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-15 05:56:59 +0000

    components(Header): use AwesomeData instead of GraphQL

--- src/components/Header/Links.jsx
+++ src/components/Header/Links.jsx
@@ -1,64 +1,44 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
 
 import Link from "../Link";
+import * as AwesomeData from "../AwesomeData";
 
 export default (props) => (
-  <StaticQuery
-    query={graphql`
-      query HeaderLinksQuery {
-        config: dataYaml {
-          color
-        }
-        allLinksYaml {
-          links: edges {
-            node {
-              title
-              link
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div css={{
-        display: "flex",
-        alignItems: "center",
-        flex: "1 1 auto",
-        overflowX: "auto",
+  <div css={{
+    display: "flex",
+    alignItems: "center",
+    flex: "1 1 auto",
+    overflowX: "auto",
+  }}>
+    <div css={{
+      display: "flex",
+      alignItems: "center",
+      marginRight: "15px",
+      zIndex: 10,
+    }}>
+      <ul css={{
+        listStyle: "none",
+        margin: 0,
+        padding: 0,
       }}>
-        <div css={{
-          display: "flex",
-          alignItems: "center",
-          marginRight: "15px",
-          zIndex: 10,
-        }}>
-          <ul css={{
-            listStyle: "none",
-            margin: 0,
-            padding: 0,
-          }}>
-            {
-              data.allLinksYaml.links.map(({ node }, i) => (
-                <Link to={ node.link } key={ i }>
-                  <li css={{
-                    padding: "10px 15px",
-                    display: "inline-block",
-                    color: "#5d7079",
-                    borderRadius: "3px",
-                    background: node.button ? "linear-gradient(45deg, #40C4FB 50%, #62D9FB)" : "none",
-                    transform: "translateZ(0px)",
-                    transition: "color .2s ease",
-                    ":hover": {
-                      color: data.config.color || "#3eb0ef",
-                    },
-                  }}>{ node.title }</li>
-                </Link>
-              ))
-            }
-          </ul>
-        </div>
-      </div>
-    )}
-  />
+        {
+          AwesomeData.links.map((link, i) => (
+            <Link to={ link.link } key={ i }>
+              <li css={{
+                padding: "10px 15px",
+                display: "inline-block",
+                color: "#5d7079",
+                borderRadius: "3px",
+                transform: "translateZ(0px)",
+                transition: "color .2s ease",
+                ":hover": {
+                  color: AwesomeData.config.color || "#3eb0ef",
+                },
+              }}>{ link.title }</li>
+            </Link>
+          ))
+        }
+      </ul>
+    </div>
+  </div>
 );
