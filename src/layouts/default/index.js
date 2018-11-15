986bc38bde3e9c6ba87f7d1fc4bb7024402200e1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-15 05:55:05 +0000

    layouts(default): use AwesomeData instead of GraphQL

--- src/layouts/default/index.js
+++ src/layouts/default/index.js
@@ -1,36 +1,24 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Main from "../../components/Main";
+import * as AwesomeData from "../../components/AwesomeData";
 
 import "./normalize.css";
 import "./index.css";
 
 export default (props) => (
-  <StaticQuery
-    query={graphql`
-      query SiteConfigQuery {
-        config: dataYaml {
-          title
-          description
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={ data.config.title }
-          description= { data.config.description }
-        />
-        <Header />
-        <Main>
-          { props.children }
-        </Main>
-        <Footer />
-      </>
-    )}
-  />
+  <>
+    <Helmet
+      title={ AwesomeData.config.title }
+      description= { AwesomeData.config.description }
+    />
+    <Header />
+    <Main>
+      { props.children }
+    </Main>
+    <Footer />
+  </>
 );
