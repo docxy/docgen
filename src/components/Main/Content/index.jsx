124f99a01a8f77d8d869adaa0b3e593e8d8faacc
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-23 07:26:51 +0000

    components(Main): don't show Feedback component if there's no feedback URL

--- src/components/Main/Content/index.jsx
+++ src/components/Main/Content/index.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import Article from "./Article";
 import Feedback from "./Feedback";
+import * as AwesomeData from "../../AwesomeData";
 
 export default (props) => (
   <div>
     <Article>
       { props.children }
     </Article>
-    <Feedback />
+    {
+      AwesomeData.config.feedback
+      ? <Feedback />
+      : <></>
+    }
   </div>
 );
