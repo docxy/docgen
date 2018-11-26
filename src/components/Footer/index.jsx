52dd9ea157d76559b2e5b0634256183a18160ade
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-26 13:30:43 +0000

    components(Footer): implement FooterLinks component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import MetaFooter from "./MetaFooter";
+import FooterLinks from "./FooterLinks";
 
 export default (props) => (
   <footer css={{
@@ -9,6 +10,7 @@ export default (props) => (
     borderTop: "1px solid #e5eff5",
     backgroundColor: "#ffffff",
   }}>
+    <FooterLinks />
     <MetaFooter />
   </footer>
 );
