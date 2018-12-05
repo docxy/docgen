52155c2b96955759c3a0ca9c777d945401855135
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-12-05 13:27:19 +0000

    html.js: use ico format for favicon

--- src/html.jsx
+++ src/html.jsx
@@ -32,7 +32,7 @@ export default class HTML extends React.Component {
           <meta property="og:type" content="website" />
 
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="shortcut icon" href="/favicon.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
