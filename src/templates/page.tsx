0b8ec167d9d3d3a4b59cd7de64b89fdd5570ea15
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-05-29 09:30:35 +0530

    templates: add page template

---
+++ src/templates/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+
+import Layout from "../layouts/index";
+
+export default (props: any) => (
+  <Layout>
+    <Helmet
+      title={ props.data.markdownRemark.frontmatter.title }
+    >
+      <meta name="description" content={ props.data.markdownRemark.frontmatter.description } />
+    </Helmet>
+    <h1>{ props.data.markdownRemark.frontmatter.title }</h1>
+    <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }} />
+  </Layout>
+);
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`;
