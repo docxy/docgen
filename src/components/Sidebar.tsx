c7764349ebf21f33d46efe4f1d290c8065983eda
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-14 08:38:44 +0530

    components(Sidebar): refactor

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,37 @@
 import { graphql, Link, StaticQuery } from "gatsby";
 import React from "react";
 
+interface ISidebarItemProps {
+  title: string;
+  link: string;
+}
+
+const SidebarItem: React.FunctionComponent<ISidebarItemProps> = ({ title, link }) => (
+  <li css={{
+    fontSize: "1.45rem",
+    lineHeight: "1.65rem",
+    "& > a.active": {
+      color: "#3eb0ef",
+      fontWeight: 500,
+    }
+  }}>
+    <Link
+      className={ typeof document !== "undefined" && document.location.pathname === link ? "active" : "" }
+      to={ link }
+      css={{
+        display: "inline-block",
+        width: "100%",
+        height: "100%",
+        padding: "6px 0",
+        color: "#738a94",
+        lineHeight: "1.5em",
+      }}
+    >
+      { title }
+    </Link>
+  </li>
+);
+
 
 export default (props) => (
   <StaticQuery
@@ -16,7 +47,7 @@ export default (props) => (
             }
           }
         }
-      }  
+      }
     `}
     render={ data => (
       <div css={{
@@ -43,30 +74,8 @@ export default (props) => (
               listStyle: "none",
             }}>
               {
-                data.allMarkdownRemark.nodes.filter(node => node.fields.slug !== "/404/").map((node: any, i: number) => (
-                  <li key={ i } css={{
-                    fontSize: "1.45rem",
-                    lineHeight: "1.65rem",
-                    "& > a.active": {
-                      color: "#3eb0ef",
-                      fontWeight: 500,
-                    }
-                  }}>
-                    <Link
-                      className={ typeof document !== "undefined" && document.location.pathname === node.fields.slug ? "active" : "" }
-                      to={ node.fields.slug }
-                      css={{
-                        display: "inline-block",
-                        width: "100%",
-                        height: "100%",
-                        padding: "6px 0",
-                        color: "#738a94",
-                        lineHeight: "1.5em",
-                      }}
-                    >
-                      { node.frontmatter.title }
-                    </Link>
-                  </li>
+                data.allMarkdownRemark.nodes.filter((node: any) => node.fields.slug !== "/404/").map((node: any, i: number) => (
+                  <SidebarItem key={ i } title={ node.frontmatter.title } link={ node.fields.slug } />
                 ))
               }
             </ol>
