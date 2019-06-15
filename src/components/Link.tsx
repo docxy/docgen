02a2a8da77748e5b744ceb80730d3b88179a1e8e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-15 10:07:16 +0530

    components: add Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link as GatsbyLink } from "gatsby";
+
+export default (props: any) => {
+  if (props.to === "") {
+    return (
+      <a { ...props }>
+        { props.children }
+      </a>
+    );
+  }
+
+  const internal = /^\/(?!\/)/.test(props.to);
+
+  if (internal) {
+    return (
+      <GatsbyLink to={ props.to } { ...props }>
+        { props.children }
+      </GatsbyLink>
+    );
+  }
+
+  return (
+    <a
+      href={ props.to }
+      target="_blank"
+      rel="noopener noreferrer"
+      { ...props }
+    >
+      { props.children }
+    </a>
+  );
+};
