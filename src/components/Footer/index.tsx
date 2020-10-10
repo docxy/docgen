fdcdd56b5cab4bf914d9fffde6da326822de7951
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-10-10 19:06:17 +0530

    components(Footer): refactor & style changes
    * Update footer style to match new base style
    * Remove feedback spot
    * Layout changes
    * Some other improvements
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,52 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+import Link from "../Link";
+
+export default (): React.ReactElement => {
+    const data = useStaticQuery(graphql`
+        query {
+            contentYaml {
+                copyright
+            }
+        }
+    `);
+
+    return (
+        <footer css={{
+            borderTop: "2px solid var(--gray6)",
+        }}>
+            <div css={{
+                maxWidth: 1300,
+                margin: "0 auto",
+                padding: "25px 50px",
+                textAlign: "center",
+                color: "var(--gray1)",
+            }}>
+                { data.contentYaml.copyright }
+            </div>
+
+            <div css={{
+                backgroundColor: "var(--black)",
+                color: "var(--white)",
+                textAlign: "center",
+                fontWeight: 500,
+            }}>
+                <div css={{
+                    maxWidth: 1300,
+                    margin: "0 auto",
+                    padding: "20px 25px",
+                }}>
+                    <Link
+                        to="https://github.com/AwesomeDocs"
+                        title="Build awesome documentation sites with AwesomeDocs"
+                        css={{
+                            opacity: .8,
+                        }}
+                    >
+                        Powered by AwesomeDocs
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
+};
