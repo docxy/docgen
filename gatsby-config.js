8a924936f966c383dcd21c3b75cda6ea569a586e
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-10-11 17:51:46 +0530

    gatsby(config): add local search plugin
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,59 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: "gatsby-plugin-local-search",
+            options: {
+                // A unique name for the search index. This should be descriptive of
+                // what the index contains. This is required.
+                name: "pages",
+                // Set the search engine to create the index. This is required.
+                // The following engines are supported: flexsearch, lunr
+                engine: "flexsearch",
+                // Provide options to the engine.
+                // Note: Only the flexsearch engine supports options.
+                engineOptions: "speed",
+                // GraphQL query used to fetch all data for the search index. This is
+                // required.
+                query: `{
+                    allMarkdownRemark {
+                        nodes {
+                            id
+                            fields{
+                                slug
+                            }
+                            frontmatter {
+                                title
+                                description
+                            }
+                            rawMarkdownBody
+                        }
+                    }
+                }`,
+                // Field used as the reference value for each document.
+                // Default: "id".
+                ref: "id",
+                // List of keys to index. The values of the keys are taken from the
+                // normalizer function below.
+                // Default: all fields
+                index: [ "title", "body", "description" ],
+                // List of keys to store and make available in the UI. The values of
+                // the keys are taken from the normalizer function below.
+                // Default: all fields
+                store: [ "id", "slug", "title", "description" ],
+                // Function used to map the result from the GraphQL query. This should
+                // return an array of items to index in the form of flat objects
+                // containing properties to index. The objects must contain the `ref`
+                // field above (default: 'id'). This is required.
+                normalizer: ({ data }) => data.allMarkdownRemark.nodes.filter(node => node.fields.slug !== "/404/").map(node => ({
+                    id: node.id,
+                    slug: node.fields.slug,
+                    title: node.frontmatter.title,
+                    description: node.frontmatter.description,
+                    body: node.rawMarkdownBody,
+                })),
+            },
+        },
         "gatsby-transformer-yaml",
         "gatsby-plugin-typescript",
     ],
