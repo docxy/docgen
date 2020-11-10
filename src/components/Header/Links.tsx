2188e048f3f858df7f409b0bd4287d10d929a91d
Ilias YAHIA <ilias.yahia1@gmail.com>
2020-11-10 20:08:18 +0100

    Visual enhacement

--- src/components/Header/Links.tsx
+++ src/components/Header/Links.tsx
@@ -2,56 +2,49 @@ import React from "react";
 import { FiCompass, FiGithub, FiTwitter } from "react-icons/fi";
 
 import HeaderLink from "./Link";
+import ThemeToggle from "./ThemeToggle";
 
 interface HeaderLinksProps {
-    repository: string;
-    twitter: string;
-    website: string;
+  repository: string;
+  twitter: string;
+  website: string;
 }
 
-export default ({ repository, twitter, website }: HeaderLinksProps): React.ReactElement => (
-    <div css={{
-        listStyle: "none",
-        display: "flex",
-        alignItems: "center",
-        fontSize: 25,
-        "@media (max-width: 768px)": {
-            display: "none",
-        },
-        // Workaround for Safari, as it doesn't support flex `gap`.
-        "& a": {
-            marginLeft: 20,
-        },
-    }}>
-        {
-            website && (
-                <HeaderLink
-                    name={ "Website" }
-                    link={ website }
-                >
-                    <FiCompass />
-                </HeaderLink>
-            )
-        }
-        {
-            repository && (
-                <HeaderLink
-                    name={ "Repository" }
-                    link={ repository }
-                >
-                    <FiGithub />
-                </HeaderLink>
-            )
-        }
-        {
-            twitter && (
-                <HeaderLink
-                    name={ "Twitter" }
-                    link={ twitter }
-                >
-                    <FiTwitter />
-                </HeaderLink>
-            )
-        }
+export default ({
+    repository,
+    twitter,
+    website,
+}: HeaderLinksProps): React.ReactElement => (
+    <div
+        css={{
+            listStyle: "none",
+            display: "flex",
+            alignItems: "center",
+            fontSize: 25,
+            "@media (max-width: 768px)": {
+                display: "none",
+            },
+            // Workaround for Safari, as it doesn't support flex `gap`.
+            "& a": {
+                marginLeft: 20,
+            },
+        }}
+    >
+        <ThemeToggle></ThemeToggle>
+        {website && (
+            <HeaderLink name={"Website"} link={website}>
+                <FiCompass />
+            </HeaderLink>
+        )}
+        {repository && (
+            <HeaderLink name={"Repository"} link={repository}>
+                <FiGithub />
+            </HeaderLink>
+        )}
+        {twitter && (
+            <HeaderLink name={"Twitter"} link={twitter}>
+                <FiTwitter />
+            </HeaderLink>
+        )}
     </div>
 );
--- src/html.tsx
+++ src/html.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 interface HTMLProps {
-    htmlAttributes: Record<string, unknown>;
-    headComponents: React.Component[];
-    bodyAttributes: Record<string, unknown>;
-    preBodyComponents: React.Component[];
-    body: string;
-    postBodyComponents: React.Component[];
+  htmlAttributes: Record<string, unknown>;
+  headComponents: React.Component[];
+  bodyAttributes: Record<string, unknown>;
+  preBodyComponents: React.Component[];
+  body: string;
+  postBodyComponents: React.Component[];
 }
 
-export default (props: React.PropsWithChildren<HTMLProps>): React.ReactElement => (
-    <html { ...props.htmlAttributes }>
+export default (
+    props: React.PropsWithChildren<HTMLProps>
+): React.ReactElement => (
+    <html lang="en" {...props.htmlAttributes}>
         <head>
             <meta charSet="utf-8" />
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
@@ -21,24 +23,43 @@ export default (props: React.PropsWithChildren<HTMLProps>): React.ReactElement =
 
             <meta name="generator" content="Docxy" />
 
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon.ico" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+            />
+            <link
+                rel="shortcut icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon.ico"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
             <link rel="manifest" href="/site.webmanifest" />
             <meta name="msapplication-TileColor" content="#000000" />
             <meta name="theme-color" content="#000000" />
 
-            { props.headComponents }
+            {props.headComponents}
         </head>
-        <body id="___docxy" { ...props.bodyAttributes }>
-            { props.preBodyComponents }
+        <body id="___docxy" {...props.bodyAttributes}>
+            {props.preBodyComponents}
             <div
-                key={ "body" }
+                key={"body"}
                 id="___gatsby"
                 dangerouslySetInnerHTML={{ __html: props.body }}
             />
-            { props.postBodyComponents }
+            {props.postBodyComponents}
         </body>
     </html>
 );
