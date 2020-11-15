d2a2e423d861b2785cddbacaa42453bd08c329ac
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-11-15 18:18:14 +0530

    components(Link): allow additional props
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- src/components/Header/Link.tsx
+++ src/components/Header/Link.tsx
@@ -5,9 +5,10 @@ import Link from "../Link";
 export interface HeaderLinkProps {
     name: string;
     link: string;
+    [key: string]: unknown;
 }
 
-export default ({ children, name, link }: React.PropsWithChildren<HeaderLinkProps>): React.ReactElement => (
+export default ({ children, name, link, ...props }: React.PropsWithChildren<HeaderLinkProps>): React.ReactElement => (
     <Link
         to={ link }
         title={ name }
@@ -17,6 +18,7 @@ export default ({ children, name, link }: React.PropsWithChildren<HeaderLinkProp
                 color: "var(--text)",
             },
         }}
+        { ...props }
     >
         { children }
     </Link>
