ba89c22a51bbf0e68f93927120ac66b93c96995b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-11-15 18:19:41 +0530

    components(ThemeToggle): refactor
    * use react-icons
    * use glamor for styling
    * add typings
    
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- src/components/Header/ThemeToggle.tsx
+++ src/components/Header/ThemeToggle.tsx
@@ -1,52 +1,25 @@
 import React from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
 
-export default (): React.ReactElement => {
-    const moonIcon = (
-        <svg
-            viewBox="0 0 24 24"
-            width="25"
-            height="25"
-            stroke-width="2"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-        </svg>
-    );
-
-    const sunIcon = (
-        <svg
-            viewBox="0 0 24 24"
-            width="25"
-            height="25"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-        >
-            <circle cx="12" cy="12" r="5"></circle>
-            <line x1="12" y1="1" x2="12" y2="3"></line>
-            <line x1="12" y1="21" x2="12" y2="23"></line>
-            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-            <line x1="1" y1="12" x2="3" y2="12"></line>
-            <line x1="21" y1="12" x2="23" y2="12"></line>
-            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-        </svg>
-    );
-
-    return (
-        <ThemeToggler>
-            {({ theme, toggleTheme }) => (
-                <div className="theme-toggle" onClick={() => toggleTheme(theme == "light" ? "dark" : "light")}>
-                    {theme === "dark" ? sunIcon : moonIcon}
+export default (): React.ReactElement => (
+    <ThemeToggler>
+        {
+            ({ theme, toggleTheme }: { theme: string; toggleTheme: (theme: string) => void }) => (
+                <div
+                    css={{
+                        transition: "color var(--transition-duration) var(--transition-function)",
+                        cursor: "pointer",
+                        color: "var(--gray1)",
+                        ":hover": {
+                            color: "var(--text)",
+                        },
+                    }}
+                    onClick={ () => toggleTheme(theme === "light" ? "dark" : "light") }
+                >
+                    { theme === "dark" ? <FiSun /> : <FiMoon /> }
                 </div>
-            )}
-        </ThemeToggler>
-    );
-};
+            )
+        }
+    </ThemeToggler>
+);
